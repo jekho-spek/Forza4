886431e02d0ae6baed98ae6fc3e660c8fd6542b1
jekho-spek <brezigargiacomo@gmail.com>
2025-11-17 17:37:27 +0100

    main
    scelta modalità

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public class Main {
         System.out.println("=== FORZA 4 ===");
         System.out.println("Scegli modalità: 1 = PvP, 2 = PvC");
         int modalita = Leggi.unInt();
+
+        while (modalita != 1 && modalita != 2) {
+            System.out.println("Inserisci 1 (PvP) o 2 (PvC)");
+            modalita = Leggi.unInt();
+        }
     }
 }
