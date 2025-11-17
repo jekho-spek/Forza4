34c676f7ff499a88239df32906825b3ea987b9fc
jekho-spek <brezigargiacomo@gmail.com>
2025-11-17 18:40:14 +0100

    cambiamento pesante

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -1,10 +1,16 @@
-public class ComputerPlayer {
-    public class ComputerPlayer extends Player {
-        private Random random;
-    
-        public ComputerPlayer(String nome, char simbolo) {
-            super(nome, simbolo);
-            random = new Random();
-        }
+import java.util.Random;
+
+public class ComputerPlayer extends Player {
+    private Random random;
+
+    public ComputerPlayer(String nome, char simbolo) {
+        // super(nome, simbolo) chiama il costruttore della classe Player
+        // e inizializza la parte "Player" di questo oggetto
+        super(nome, simbolo);
+        random = new Random();
     }
+
+    // Sceglie una colonna valida a caso (1-7)
+    public int scegliColonna(Scacchiera scacchiera) {
 }
+
