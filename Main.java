f480eba22c7657ca7893a68cd774e15e6f7047fb
jekho-spek <brezigargiacomo@gmail.com>
2025-11-17 17:31:49 +0100

    avevo sbagliato chill

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== FORZA 4 ===");
+        System.out.println("Scegli modalità: 1 = PvP, 2 = PvC");
+        int modalita = Leggi.unInt();
+    }
+}
