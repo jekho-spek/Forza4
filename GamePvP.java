b7c61e032675ba93c495be4e87aba40ef3ecb0e9
jekho-spek <brezigargiacomo@gmail.com>
2025-12-04 17:11:00 +0100

    fixato gamepvc e gamepvp

--- GamePvP.java
+++ GamePvP.java
@@ -1,46 +1,52 @@
 public class GamePvP {
-    private Scacchiera scacchiera;
-    private Player giocatore1;
-    private Player giocatore2;
+
+    private final Scacchiera scacchiera = new Scacchiera();
+    private final Player giocatore1;
+    private final Player giocatore2;
 
     public GamePvP(String nome1, String nome2) {
-        scacchiera = new Scacchiera();
         giocatore1 = new Player(nome1, 'X');
         giocatore2 = new Player(nome2, 'O');
     }
 
     public void avvia() {
         Player corrente = giocatore1;
-        boolean finito = false;
         scacchiera.inizializza();
 
-        while (!finito) {
+        while (true) {
+
             scacchiera.stampa();
+
             System.out.println("Turno di " + corrente.getNome() + " (" + corrente.getSimbolo() + ")");
             System.out.print("Scegli la colonna (1-7): ");
 
-            int colonna = Leggi.unInt();
-            boolean mossaValida = scacchiera.inserisciPedina(colonna, corrente.getSimbolo());
-
-            while (!mossaValida) {
-                System.out.print("Inserisci una colonna valida (1-7): ");
-                colonna = Leggi.unInt();
-                mossaValida = scacchiera.inserisciPedina(colonna, corrente.getSimbolo());
-            }
+            int colonna = leggiColonnaValida();
+            scacchiera.inserisciPedina(colonna, corrente.getSimbolo());
 
             if (scacchiera.controlloVittoria(corrente.getSimbolo())) {
                 scacchiera.stampa();
                 System.out.println(corrente.getNome() + " ha vinto!");
-                finito = true;
-            } else if (scacchiera.isPiena()) {
+                break;
+            }
+
+            if (scacchiera.isPiena()) {
                 scacchiera.stampa();
                 System.out.println("Pareggio!");
-                finito = true;
-            } else {
-                corrente = (corrente == giocatore1) ? giocatore2 : giocatore1;
+                break;
             }
+
+            corrente = (corrente == giocatore1) ? giocatore2 : giocatore1;
         }
 
         System.out.println("=== FINE PARTITA ===");
+    }
+
+    private int leggiColonnaValida() {
+        int col = Leggi.unInt();
+        while (!scacchiera.colonnaValida(col)) {
+            System.out.print("Colonna invalida. Riprova: ");
+            col = Leggi.unInt();
+        }
+        return col;
     }
 }
